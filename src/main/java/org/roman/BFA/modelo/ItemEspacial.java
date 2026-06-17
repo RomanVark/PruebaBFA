d664e4e68ed8f8f2ce6465866617bca8a1613c50
RomanVark <jrromero@uamv.edu.ni>
2026-06-17 15:23:09 -0600

    ItemEspacial completado

--- src/main/java/org/roman/BFA/modelo/ItemEspacial.java
+++ src/main/java/org/roman/BFA/modelo/ItemEspacial.java
@@ -1,4 +1,39 @@
 package org.roman.BFA.modelo;
 
+import lombok.Getter;
+import lombok.Setter;
+import net.sf.jasperreports.data.hibernate.spring.SpringHibernateDataAdapter;
+import org.openxava.annotations.Required;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+
 public class ItemEspacial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Required
+    private int Numero;
+
+    @Column(length = 200)
+    private String ImagenModelo;
+
+    @Column(length = 200)
+    private String ImagenA;
+    @Column(length = 200)
+    private String ImagenB;
+    @Column(length = 200)
+    private String ImagenC;
+    @Column(length = 200)
+    private String ImagenD;
+    @Column(length = 200)
+    private String ImagenE;
+
+    @Required
+    @Column(length = 5)
+    private String RespuestaCorrecta;
 }
