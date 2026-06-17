ed6f12854dece68767a9b962772abebadefe98b8
RomanVark <jrromero@uamv.edu.ni>
2026-06-16 21:02:32 -0600

    Evaluado completo

--- src/main/java/org/roman/BFA/modelo/Evaluado.java
+++ src/main/java/org/roman/BFA/modelo/Evaluado.java
@@ -1,4 +1,43 @@
 package org.roman.BFA.modelo;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.openxava.annotations.Required;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter
+
 public class Evaluado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Required
+    @Column(length = 50)
+    private String primerApellido;
+
+    @Required
+    @Column(length = 50)
+    private String segundoApellido;
+
+    @Required
+    @Column(length = 50)
+    private String nombre;
+
+    private LocalDate fechaNacimiento;
+
+    private int edad;
+
+    @Column(length = 50)
+    private String sexo;
+
+    @Column(length = 100)
+    private String estudiosRealizados;
+
+    @Column(length = 100)
+    private String profesion;
 }
